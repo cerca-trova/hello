c7f38cc2752068ea776bf9fbe51001d5152730ce
chenran <chenran@nawaa.com>
2021-11-12 17:00:12 +0800

    test map

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cerca-trova/hello
 
 go 1.17
 
-require github.com/cerca-trova/greetings v0.0.0-20211112053130-17f4ece88a26
+require github.com/cerca-trova/greetings v0.0.0-20211112080154-aa56c39fe90e
--- hello.go
+++ hello.go
@@ -11,7 +11,8 @@ func main() {
 	log.SetPrefix("greetings:")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("chenran")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
